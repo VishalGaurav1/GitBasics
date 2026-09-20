206cfad1744164e1f4ce6604b73c916dd159e402
VishalGaurav1 <gaurav.vishal790@gmail.com>
2026-09-20 11:50:52 +0530

    Second Commit

--- src/main.py
+++ src/main.py
@@ -6,8 +6,14 @@ class Calculator:
     def multiply(self,a,b):
         return a*b
 
+    def divide(self,a,b):
+        if b==0:
+            return 'Error:Division by 0'
+        return a/b
+
 
 if __name__=="__main__":
     calc=Calculator()
     print(f"2+3={calc.add(2,3)}")
     print(f"3*4={calc.multiply(3,4)}")
+    print(f"10/2={calc.divide(10,2)}")
